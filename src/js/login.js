b4d7c9578c394dc7a2e1947fd4570c45dd0eba2c
ruan <ruanlourencosilva.rj@gmail.com>
2024-12-11 14:23:20 -0300

    login page validation done

---
+++ src/js/login.js
@@ -0,0 +1,41 @@
+const email = document.getElementById("mail");
+const password = document.getElementById("password");
+const submit = document.getElementById("submit");
+
+const emailValidationMessage = document.getElementById("email-validation-message");
+const passwordValidationMessage = document.getElementById("password-validation-message")
+
+var form = document.getElementById("login");
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
+
+
+const ValidEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+
+
+submit.addEventListener("click" , (event) => 
+{    
+    let emailPass = false;
+    let passwordPass = false;
+
+    if(email.value == "")
+        emailValidationMessage.innerHTML = "campo não preenchido"
+    else
+    {
+        if(email.value.match(ValidEmail))
+            {emailValidationMessage.innerHTML = "";
+                emailPass =true;}
+        else
+            emailValidationMessage.innerHTML = "Email Invalido";
+    }
+
+    if(password.value === "")
+        passwordValidationMessage.innerHTML = "campo não preenchido";
+    else
+        passwordPass = true;
+
+    if(passwordPass && emailPass)
+        window.location.href = "kanban.html"
+
+})
